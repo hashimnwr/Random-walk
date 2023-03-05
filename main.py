e5a06bedce3277f11d489467644c2b3009a58676
Mohammed Hashim Anwar <84743105+hashimnwr@users.noreply.github.com>
2023-03-05 05:13:07 +0300

    Random walk version 1.0

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+from turtle import Turtle, Screen
+from random import choice, randint
+
+brush = Turtle()
+screen = Screen()
+
+brush.hideturtle()
+
+i = 0
+
+turtle.colormode(255)
+
+
+def change_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
+
+brush.width(7)
+
+angle = [0, 90, 180, 270]
+brush.speed('fastest')
+
+while i < 300:
+    brush.color(change_color())
+    brush.forward(20)
+    brush.setheading(choice(angle))
+    i += 1
+
+screen.exitonclick()
